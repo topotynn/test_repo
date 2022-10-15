c62ee0e8934a81547c49f57da88cc033f13b7f7e
Andrii Uharov <uharova@mydigicode.com>
2022-10-15 11:56:14 +0300

    dev2

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -19,3 +19,5 @@ provider "kubernetes" {
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = "${base64decode(google_container_cluster.mogo_dev_tf.master_auth.0.cluster_ca_certificate )}"
 }
+
+data "google_client_config" "default" {}
